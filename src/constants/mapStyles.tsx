6adee95bfdacd993001083cd732d6e3942fa3e26
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2025-12-11 15:33:47 +0900

    code : 마커 추가

--- src/constants/mapStyles.tsx
+++ src/constants/mapStyles.tsx
@@ -3,10 +3,12 @@ import { mapSpaceColors } from "./colorPalette";
 const floorSrc = "floors";
 const wallSrc = "walls";
 const roomSrc = "rooms";
+const pointSrc = "points";
 
 const floorSource: [string, mapboxgl.SourceSpecification] = [floorSrc, { type: "geojson", data: "data/fdx_floors.geojson" }];
 const wallSource: [string, mapboxgl.SourceSpecification] = [wallSrc, { type: "geojson", data: "data/fdx_walls.geojson" }];
 const roomSource: [string, mapboxgl.SourceSpecification] = [roomSrc, { type: "geojson", data: "data/fdx_spaces.geojson" }];
+const pointSource1: [string, mapboxgl.SourceSpecification] = [pointSrc, { type: "geojson", data: "data/points.geojson" }];
 
 const floorLayer: mapboxgl.FillExtrusionLayerSpecification = {
   id: `${floorSrc}-layer`,
@@ -33,5 +35,46 @@ const roomLayer: mapboxgl.FillExtrusionLayerSpecification = {
   },
 };
 
-export const mapSources = [floorSource, wallSource, roomSource];
-export const mapLayers = [floorLayer, wallLayer, roomLayer];
+const pointLayer0: mapboxgl.SymbolLayerSpecification = {
+  id: `${pointSrc}0-layer`,
+  source: pointSrc,
+  type: "symbol",
+  layout: {
+    "icon-image": "icon-bg",
+    "icon-allow-overlap": true,
+    "icon-size": 0.17,
+    "text-size": 0,
+  },
+  paint: {
+    "icon-color": "#ffffff",
+  },
+  minzoom: 17,
+};
+
+const pointLayer1: mapboxgl.SymbolLayerSpecification = {
+  id: `${pointSrc}1-layer`,
+  source: pointSrc,
+  type: "symbol",
+  layout: {
+    "icon-image": "meeting-room",
+    "icon-size": 0.17,
+    "text-field": ["get", "title"],
+    "text-font": ["Open Sans Regular"],
+    "text-offset": [0, 1.1],
+    "text-anchor": "top",
+    "text-size": 13,
+    "icon-allow-overlap": true,
+    "text-allow-overlap": false,
+    "text-optional": true, // text-optional아 고맙다...
+  },
+  paint: {
+    "icon-color": "#2F3E87",
+    "text-color": "#090E20",
+    "text-halo-color": "#ffffff",
+    "text-halo-width": 1.5,
+  },
+  minzoom: 17,
+};
+
+export const mapSources = [floorSource, wallSource, roomSource, pointSource1];
+export const mapLayers = [floorLayer, wallLayer, roomLayer, pointLayer0, pointLayer1];
--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -1,12 +1,14 @@
 import mapboxgl from "mapbox-gl";
 import { useEffect, useRef, useState } from "react";
 import { mapLayers, mapSources } from "../constants/mapStyles";
+import meetingImg from "/meeting-icon.png";
+import iconBgImg from "/icon-bg.png";
 
 mapboxgl.accessToken = `${import.meta.env.VITE_MAPBOXGL_ACCESS_TOKEN}`;
 
 const useMap = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const [map, setMap] = useState<mapboxgl.Map | null>();
+  const [, setMap] = useState<mapboxgl.Map | null>();
 
   const initMap = () => {
     if (!containerRef.current) return;
@@ -18,21 +20,28 @@ const useMap = () => {
       pitch: 50,
     });
     setMap(map);
+    // eslint-disable-next-line react-hooks/immutability
     map.on("click", (e) => onClickMap(e));
     map.on("load", () => {
-      mapSources.forEach((source) => map.addSource(...source));
-      mapLayers.forEach((layer) => map.addLayer(layer));
+      map.loadImage(meetingImg, (_, image1) => {
+        map.loadImage(iconBgImg, (_, image2) => {
+          if (!image1 || !image2) return;
+          map.addImage("meeting-room", image1, { sdf: true });
+          map.addImage("icon-bg", image2, { sdf: true });
+          mapSources.forEach((source) => map.addSource(...source));
+          mapLayers.forEach((layer) => map.addLayer(layer));
+        });
+      });
     });
     return () => map.remove();
   };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(initMap, []);
 
   const onClickMap = (e: mapboxgl.MapMouseEvent) => {
-    if (!map) return;
-    const features = map.queryRenderedFeatures(e.point, { layers: ["bld1"] });
-    const targetIdx = features && features.length > 0 && features[0].layer!.id === "bld1" ? features[0].properties!.idx : undefined;
-    console.log(targetIdx);
+    console.log(e.lngLat.lng, e.lngLat.lat);
+    // const features = map.queryRenderedFeatures(e.point, { layers: ["bld1"] });
+    // const targetIdx = features && features.length > 0 && features[0].layer!.id === "bld1" ? features[0].properties!.idx : undefined;
+    // console.log(targetIdx);
   };
 
   return { containerRef };
